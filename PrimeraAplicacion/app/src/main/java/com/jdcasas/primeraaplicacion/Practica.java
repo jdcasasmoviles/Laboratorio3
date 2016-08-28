f49d9ccef80ba84bdd1f5534e536e361523a565a
jdcasasmoviles <jdcasasmoviles@gmail.com>
2016-08-28 01:48:37 -0500

    Ejecutando nuestra primera aplicación

---
+++ PrimeraAplicacion/app/src/main/java/com/jdcasas/primeraaplicacion/Practica.java
@@ -0,0 +1,31 @@
+package com.jdcasas.primeraaplicacion;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by Usuario on 27/08/2016.
+ */
+
+public class Practica extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practica);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
